4af872719432a3de494687bc868b491e5f00d2f3
Hyunwoo Moon <mhw9163@gmail.com>
2021-04-05 10:17:27 -0700

    deploying

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,14 +1,15 @@
 /* eslint-disable no-undef */
 require("dotenv").config();
 
-const pg = require('pg');
-pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
+const pg = require("pg");
+pg.defaults.ssl =
+  process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
 
 module.exports = {
   migrationDirectory: "migrations",
   driver: "pg",
   connectionString:
-    process.env.NODE_ENV === "test"
+    process.NODE_ENV === "test"
       ? process.env.TEST_DATABASE_URL
       : process.env.DATABASE_URL,
 };
